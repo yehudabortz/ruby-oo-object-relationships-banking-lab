ca63b70002cd7b56e73603017ca8471d6bcfec92
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-12 14:09:51 -0600

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,9 +11,22 @@ class Transfer
   end
 
   def execute_transaction
+    if self.status == "pending" && valid? && sender.balance > amount 
     sender.balance -= amount
     receiver.balance += amount
     self.status = "complete"
+    else
+      self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+    receiver.balance -= amount
+    sender.balance += amount
+    self.status = "reversed"
+    end
   end
  
 end
